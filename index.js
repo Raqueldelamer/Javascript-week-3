b8ee6217067927a431558041bac34d30cd07968f
Raquel Marie <raqueldelamer@gmail.com>
2024-08-12 16:02:17 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const pet = {
+    name: "Minou",
+    species: "cat",
+    happiness: "95", 
+};
+
+const pet2 = {};
+// Add Properties
+pet2.Name = "Sheba";
+pet2.Species = "cat";
+pet2.Happiness = "70";
+
+
+// great js object examples https://www.w3schools.com/js/js_objects.asp
