a8ae07e2ddccbbdd857c2300b3fa825cffd2e6a5
Raquel Marie <raqueldelamer@gmail.com>
2024-08-12 19:57:18 -0500

    update

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const pet = {
     happiness: "50", 
 };
 
+console.log(pet.name);
+console.log(pet.species);
+console.log(pet.happiness);
+
+
+
 const pet2 = {};
 // Add Properties
 pet2.Name = "Sheba";
 pet2.Species = "cat";
 pet2.Happiness = "50";
+
+console.log(pet2.Name);
+console.log(pet2.Species);
+console.log(pet2.Happiness);
 
 
 // great js object examples https://www.w3schools.com/js/js_objects.asp
