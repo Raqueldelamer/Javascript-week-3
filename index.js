50ad57cda7040ee57308e24400ddd18c0e7cd42f
Raquel Marie <raqueldelamer@gmail.com>
2024-08-13 21:38:36 -0500

    update js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const pet = {
     name: "Minou",
     species: "cat",
-    happiness: "50",
-
-
- "petPlay": function() {
-    console.log(`Pet name: ${this.name}, Species: ${this.species}, Happiness: ${this.happiness}`);
-    console.log(`this.happiness+=10`);
- },
-
- "petFeed": function(){
-    console.log(`Pet Status..`)
-    
- },
- "petStatus": function(){
-
+    happiness: 50,
+    addHappiness: function() {
+      this.happiness += 10;
+    },
+   status: function() {
+      console.log(`Pet name: ${this.name}, Species: ${this.species}, Happiness: ${this.happiness}`);
+      console.log(`${this.name} has played and been fed!`);
+      console.log(`${this.name} is at level ${this.happiness} of happiness.`);
    }
 } 
+
+pet.status();
+pet.addHappiness();
+pet.status();
+pet.addHappiness();
+pet.status();
+pet.addHappiness();
+pet.status();
+
 //console.log(`${pet.name} is ${pet.happiness}`);
 //pet.fed();
 //pet.happy();
@@ -26,10 +29,14 @@ const pet = {
 // function add(happiness, status) {
  //   return happiness+status;
 // }
-console.log(pet.petPlay());
+// console.log(pet.petPlay());
 console.log(pet.name);
 console.log(pet.species);
 console.log(pet.happiness);
+// console.log("\nToDo 2: City")
+// city.status();
+// city.addPop();
+// city.status();
 
 
 
